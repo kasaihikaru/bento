7ee21f4c67253dc82765cbc8e9b73027f8a897a0
Hikaru Kasai <fkrrkf@gmail.com>
2017-02-18 21:47:49 +0900

    ルートパスの指定場所変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "sentence#index"
   devise_for :users
+  root to: "sentence#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
