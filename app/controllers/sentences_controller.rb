dacb651ce0b8475b78c548e6266813c1efa8f264
Hikaru Kasai <fkrrkf@gmail.com>
2017-02-24 13:56:34 +0900

    投稿機能 文章

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,11 +1,23 @@
 class SentencesController < ApplicationController
 
   def index
-    
+
   end
 
   def edit
-    
   end
+
+
+  def create
+    Sentence.create(create_params)
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def create_params
+    params.require(:sentence).permit(:ja, :en).merge(user_id: current_user.id)
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
 
   def show
-    @fold=Fold.new
+    @sentence = Sentence.new
   end
 
   def edit
     
+  end
+
+  def create
+    Sentence.create(create_params)
+  end
+
+  private
+
+  def create_params
+    params.require(:sentence).permit(:ja, :en).merge(user_id: current_user.id)
   end
 
 end
